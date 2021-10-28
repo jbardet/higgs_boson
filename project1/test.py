d0dcb607a26d4626f5594cb12d47e97d31384b99
ndelamaide <nicolas.delamaide@gmail.com>
2021-10-28 10:21:04 +0200

    update

--- project1/test.py
+++ project1/test.py
@@ -1,7 +1,7 @@
 from operator import truediv
 import numpy as np
 from scripts.proj1_helpers import *
-from scripts.implementations import ridge_regression, reg_logistic_regression
+from scripts.implementations import ridge_regression, logistic_regression, least_squares, least_squares_GD
 
 DATA_TRAIN_PATH = 'data/train.csv'
 y, tx, ids = load_csv_data(DATA_TRAIN_PATH)
@@ -14,19 +14,19 @@ OUTPUT_PATH = 'outputs/test.txt'
 x_train, x_test, y_train, y_test = split_data(tx, y, 0.7)
 
 
-max_iters = 100
-gamma = 1e-5
+max_iters = 150
+gamma = 1e-1
 lambda_ = 1e-4
 
 x_train_cleaned, _, _ = modify_missing_data(x_train, -999, 0.9, x_train)
 x_test_cleaned, _, _ = modify_missing_data(x_test, -999, 0.9, x_train)
 
-x_train_cleaned_normalized, x_test_cleaned_normalized = normalize_cat(x_train_cleaned, x_test_cleaned)
+x_train_cleaned_normalized, x_test_cleaned_normalized = standardize_cat(x_train_cleaned, x_test_cleaned)
 
 x_train_onehot = one_hot(x_train_cleaned_normalized)
 x_test_onehot = one_hot(x_test_cleaned_normalized)
 
-degree = 11
+degree = 2
 
 x_train_poly = build_poly(x_train_onehot, degree)
 x_test_poly = build_poly(x_test_onehot, degree)
@@ -34,7 +34,7 @@ x_test_poly = build_poly(x_test_onehot, degree)
 initial_w = np.zeros(x_train_poly.shape[1])
 
 ## Test on some small data
-weights, loss = ridge_regression(y_train, x_train_poly, lambda_)
+weights, loss = least_squares_GD(y_train, x_train_poly, initial_w, max_iters, gamma)
 y_pred = predict_labels(weights, x_test_poly)
 
 accuracy = np.sum(y_pred==y_test) / y_pred.shape[0]
