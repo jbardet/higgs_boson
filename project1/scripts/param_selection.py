eeffbaf11125135e75f3b1c8b0df8fd86201662f
ndelamaide <nicolas.delamaide@gmail.com>
2021-10-28 11:11:13 +0200

    to compare

--- project1/scripts/param_selection.py
+++ project1/scripts/param_selection.py
@@ -1,6 +1,6 @@
 import numpy as np
-from proj1_helpers import *
-from implementations import loss_logistic, logistic_regression, reg_logistic_regression, least_squares_GD, least_squares_SGD, ridge_regression
+from proj1_helpers import standardize_all, standardize_cat, build_k_indices, compute_mse, modify_missing_data, predict_labels, predict_labels_logistic
+from implementations import loss_logistic, logistic_regression, reg_logistic_regression, least_squares_GD, least_squares_SGD, least_squares, ridge_regression
 
 
 def gamma_grid_search(y, tx, gammas, k_fold, seed=101):
@@ -27,7 +27,7 @@ def gamma_grid_search(y, tx, gammas, k_fold, seed=101):
             y_train = y[train_indice]
             x_test = tx[test_indice]
             x_train = tx[train_indice]
-            x_train, x_test = normalize_all(x_train, x_test)
+            x_train, x_test = standardize_all(x_train, x_test)
             initial_w = np.zeros(x_train.shape[1])
 
             # for log reg add offset term w0
@@ -79,7 +79,7 @@ def lambda_grid_search(y, tx, lambdas, k_fold, seed=101):
             y_train = y[train_indice]
             x_test = tx[test_indice]
             x_train = tx[train_indice]
-            x_train, x_test = normalize_all(x_train, x_test)
+            x_train, x_test = standardize_all(x_train, x_test)
             initial_w = np.zeros(x_train.shape[1])
 
             # for log reg
@@ -110,41 +110,63 @@ def compare_models(y, tx, gammas, lambdas, k_fold, seed=101):
     acc_logreg_reg = []
     acc_ls_GD = []
     acc_ls_SGD = []
+    acc_ls = []
     acc_ridge = []
 
     for k in range(k_fold):
+
         test_indice = k_indices[k]
         train_indice = k_indices[~(np.arange(k_indices.shape[0]) == k)].reshape(-1)
+
         y_test = y[test_indice]
         y_train = y[train_indice]
         x_test = tx[test_indice]
         x_train = tx[train_indice]
-        x_train, x_test = normalize_all(x_train, x_test)
-        initial_w = np.zeros(x_train.shape[1])
 
-        w, _ = logistic_regression(y_train, x_train, initial_w, 100, gammas[0])
-        y_test_pred = predict_labels_logistic(w, x_test)
+        x_train_cleaned, _, _ = modify_missing_data(x_train, -999, 0.9, x_train)
+        x_test_cleaned, _, _ = modify_missing_data(x_test, -999, 0.9, x_train)
+
+        x_train_cleaned_normalized, x_test_cleaned_normalized = standardize_cat(x_train_cleaned, x_test_cleaned) # Diff avec standardize all
+
+        print(x_train_cleaned_normalized.shape, x_test_cleaned_normalized.shape)
+
+        #x_train_onehot = one_hot(x_train_cleaned_normalized)
+        #x_test_onehot = one_hot(x_test_cleaned_normalized)
+
+        x_train_onehot = x_train_cleaned_normalized
+        x_test_onehot = x_test_cleaned_normalized
+
+        initial_w = np.zeros(x_train_onehot.shape[1])
+
+        w, _ = logistic_regression(y_train, x_train_onehot, initial_w, 100, gammas[0])
+        y_test_pred = predict_labels_logistic(w, x_test_onehot)
         acc_test = np.sum(y_test_pred==y_test) / y_test_pred.shape[0]
         acc_logreg.append(acc_test)
 
-        w, _ = reg_logistic_regression(y_train, x_train, lambdas[0], initial_w, 100, gammas[1])
-        y_test_pred = predict_labels_logistic(w, x_test)
+        w, _ = reg_logistic_regression(y_train, x_train_onehot, lambdas[0], initial_w, 100, gammas[1])
+        y_test_pred = predict_labels_logistic(w, x_test_onehot)
         acc_test = np.sum(y_test_pred==y_test) / y_test_pred.shape[0]
         acc_logreg_reg.append(acc_test)
 
-        w, _ = least_squares_GD(y_train, x_train, initial_w, 100, gammas[2])
-        y_test_pred = predict_labels(w, x_test)
+        w, _ = least_squares_GD(y_train, x_train_onehot, initial_w, 100, gammas[2])
+        y_test_pred = predict_labels(w, x_test_onehot)
         acc_test = np.sum(y_test_pred==y_test) / y_test_pred.shape[0]
         acc_ls_GD.append(acc_test)
 
-        w, _ = least_squares_SGD(y_train, x_train, initial_w, 150, gammas[3])
-        y_test_pred = predict_labels(w, x_test)
+        w, _ = least_squares_SGD(y_train, x_train_onehot, initial_w, 150, gammas[3])
+        y_test_pred = predict_labels(w, x_test_onehot)
         acc_test = np.sum(y_test_pred==y_test) / y_test_pred.shape[0]
         acc_ls_SGD.append(acc_test)
 
-        w, _ = ridge_regression(y_train, x_train, lambdas[1])
-        y_test_pred = predict_labels(w, x_test)
+        w, _ = least_squares(y_train, x_train_onehot)
+        y_test_pred = predict_labels(w, x_test_onehot)
         acc_test = np.sum(y_test_pred==y_test) / y_test_pred.shape[0]
+        acc_ls.append(acc_test)
+
+        w, _ = ridge_regression(y_train, x_train_onehot, lambdas[1])
+        y_test_pred = predict_labels(w, x_test_onehot)
+        acc_test = np.sum(y_test_pred==y_test) / y_test_pred.shape[0]
+        print(acc_test)
         acc_ridge.append(acc_test)
 
-    return np.mean(acc_logreg, axis=0), np.mean(acc_logreg_reg, axis=0), np.mean(acc_ls_GD, axis=0), np.mean(acc_ls_SGD, axis=0), np.mean(acc_ridge, axis=0)
+    return np.mean(acc_logreg, axis=0), np.mean(acc_logreg_reg, axis=0), np.mean(acc_ls_GD, axis=0), np.mean(acc_ls_SGD, axis=0), np.mean(acc_ls, axis=0), np.mean(acc_ridge, axis=0)
