3f92aea75b762b67ecc22906d14997665001884b
jbardet <44366181+jbardet@users.noreply.github.com>
2021-10-29 07:42:59 +0200

    ROC

--- project1/test.py
+++ project1/test.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scripts.proj1_helpers import *
 from scripts.implementations import ridge_regression, logistic_regression, least_squares, least_squares_SGD,least_squares_GD
+from roc_auc import roc
 
 DATA_TRAIN_PATH = 'data/train.csv'
 y, tx, ids = load_csv_data(DATA_TRAIN_PATH)
@@ -24,12 +25,13 @@ degree = 11
 x_train_poly = build_poly(x_train_onehot, degree)
 x_test_poly = build_poly(x_test_onehot, degree)
 
-
 initial_w = np.zeros(x_train_poly.shape[1])
 
 ## Test on some small data
 weights, loss = ridge_regression(y_train, x_train_poly, lambda_)
 y_pred = predict_labels(weights, x_test_poly)
+
+roc(x_test_poly, y_test, weights, [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
 
 accuracy = np.sum(y_pred==y_test) / y_pred.shape[0]
 print("Normalized + cleaned", loss, accuracy)
