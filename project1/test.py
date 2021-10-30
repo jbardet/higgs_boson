d936a921ae4b7f14e32af377262fc0b425997bb7
ndelamaide <nicolas.delamaide@gmail.com>
2021-10-30 09:51:52 +0200

    poly

--- project1/test.py
+++ project1/test.py
@@ -8,7 +8,7 @@ y, tx, ids = load_csv_data(DATA_TRAIN_PATH)
 x_train, x_test, y_train, y_test = split_data(tx, y, 0.7)
 
 max_iters = 100
-gamma = 1e-3
+gamma = 1e-5
 lambda_ = 1e-4
 
 x_train_cleaned, _, _ = modify_missing_data(x_train, -999, 0.9, x_train)
@@ -19,17 +19,16 @@ x_train_cleaned_normalized, x_test_cleaned_normalized = standardize_cat(x_train_
 x_train_onehot = one_hot(x_train_cleaned_normalized)
 x_test_onehot = one_hot(x_test_cleaned_normalized)
 
-degree = 11
+degree = 1
 
 x_train_poly = build_poly(x_train_onehot, degree)
 x_test_poly = build_poly(x_test_onehot, degree)
 
-
 initial_w = np.zeros(x_train_poly.shape[1])
 
 ## Test on some small data
-weights, loss = ridge_regression(y_train, x_train_poly, lambda_)
-y_pred = predict_labels(weights, x_test_poly)
+weights, loss = logistic_regression(y_train, x_train_poly, initial_w, max_iters, gamma)
+y_pred = predict_labels_logistic(weights, x_test_poly)
 
 accuracy = np.sum(y_pred==y_test) / y_pred.shape[0]
 print("Normalized + cleaned", loss, accuracy)
